5c3880b72fc33a518b1dba0e3b8233025c4ad64a
Daniel Martin <dmarting@vicomtech.org>
2024-01-23 16:55:29 +0100

    added something idk

--- app/src/main/java/com/example/interfaz_mvil/mapa/MapsActivity.java
+++ app/src/main/java/com/example/interfaz_mvil/mapa/MapsActivity.java
@@ -3,8 +3,13 @@ package com.example.interfaz_mvil.mapa;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.interfaz_mvil.R;
+import com.example.interfaz_mvil.apistuff_camara.ApiService_camara;
+import com.example.interfaz_mvil.apistuff_camara.camara;
+import com.example.interfaz_mvil.apistuff_camara.camararesponse;
+import com.example.interfaz_mvil.recyclerview_camaras.Adapter_camaras;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -13,10 +18,21 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.interfaz_mvil.databinding.ActivityMapsBinding;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+
+    private Adapter_camaras adapterCamaras;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +45,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.euskadi.eus/traffic/v1.0/") // Replace with your base URL
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        ApiService_camara apiServiceCamera = retrofit.create(ApiService_camara.class);
+
+
+        apiServiceCamera.getCameras(1).enqueue(new Callback<camararesponse>() {
+            @Override
+            public void onResponse(Call<camararesponse> call, Response<camararesponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<camara> cameras = response.body().getCamaras();
+
+
+                } else {
+                    // Handle the case where the response was not successful
+                    Log.e("API Error", "Response not successful: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<camararesponse> call, Throwable t) {
+                // Handle the case where the request failed to execute
+                Log.e("API Error", "Request failed", t);
+            }
+        });
+
+
+
+
+
+
+
+
     }
 
     /**
